5e2c13801b39a6ba197acbd7cc0f3b8ac4534a5d
ayaxxo <107775834+ayaxxo@users.noreply.github.com>
2022-07-03 20:38:27 +0300

    task 1 and bonus are done

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function BMI(weight,height){
+   return weight/(height*height);
+}
+let bmi=BMI(52,162);
+console.log(bmi);
+
+
+
+
+//bonus 1
+
+// function BMI(weight=52,height=162){
+//     return weight/(height*height);
+//  }
+//  let bmi=BMI();
+//  console.log(bmi);
+ 
+ 
