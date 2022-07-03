f098f0d8f998ab645754f422e20c11200c0cb703
ayaxxo <107775834+ayaxxo@users.noreply.github.com>
2022-07-03 21:05:10 +0300

    Task 2 and bonus are done

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 function BMI(weight,height){
    return weight/(height*height);
 }
+
 let bmi=BMI(52,1.62);
 console.log(bmi);
-
 
 //bonus 1
 
@@ -17,7 +17,13 @@ console.log(bmi);
 
 
  function Status(bmi){
-
-
+    if (bmi <18.5){
+        return 'لديك نقص في الوزن';
+    }
+    else if (bmi>=18.5 && bmi<25){
+        return 'وزنك صحي'
+    }
+    else if (bmi>=25){
+        return 'لديك زيادة في الوزن'
+    }
  }
- 
