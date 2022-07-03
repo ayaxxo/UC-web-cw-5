adcccb7181b20678dd38f15d3692cb0044932182
ayaxxo <107775834+ayaxxo@users.noreply.github.com>
2022-07-04 00:38:32 +0300

    task 3 and bonus are finallly doneee

--- script.js
+++ script.js
@@ -21,9 +21,32 @@ console.log(bmi);
         return 'لديك نقص في الوزن';
     }
     else if (bmi>=18.5 && bmi<25){
-        return 'وزنك صحي'
+        return 'وزنك صحي';
     }
     else if (bmi>=25){
-        return 'لديك زيادة في الوزن'
+        return 'لديك زيادة في الوزن';
     }
+ } }
+
+
+ function calculate(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+    let bmi = BMI(weight,height);
+    let desc = Status(bmi);
+    
+document.getElementById("result").innerHTML = `<h2>your BMI is ${bmi} <br> ${desc}</h2>`;
+if (desc=='لديك نقص في الوزن'){
+    document.getElementById("result").style.color="orange";
+}
+else if (desc == 'وزنك صحي'){
+    document.getElementById("result").style.color="green";
+}
+else if (desc=='لديك زيادة في الوزن'){
+    document.getElementById("result").style.color="red";
+
+}
+console.log(`your BMI is ${bmi} and ${desc}`);
+
+
  }
